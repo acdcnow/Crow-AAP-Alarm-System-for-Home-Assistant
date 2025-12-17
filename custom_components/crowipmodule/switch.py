c2a1a49495671afd3a7d6d5a8c5f72c584e29e85
acdcnow <33558339+acdcnow@users.noreply.github.com>
2025-12-17 14:43:20 +0100

    Update switch.py

--- custom_components/crowipmodule/switch.py
+++ custom_components/crowipmodule/switch.py
@@ -1,128 +1,131 @@
-"""Support for Crow IP 8/16 Module sensors (shows panel info)."""
+"""Support for Crow IP Module switches (Outputs & Relays)."""
 import logging
+from typing import Any
 
-from homeassistant.core import callback
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.components.switch import SwitchEntity
-from homeassistant.const import STATE_OFF, STATE_ON
-from homeassistant.components import logbook
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from homeassistant.const import (
-    STATE_OFF,
-    STATE_ON,
-    STATE_UNKNOWN,
-    STATE_UNAVAILABLE,
-)
-from . import (
-    CONF_OUTPUTNAME,
-    DATA_CRW,
-    OUTPUT_SCHEMA,
+from .const import (
+    DOMAIN,
     SIGNAL_OUTPUT_UPDATE,
-    CrowIPModuleDevice,
+    CONF_OUTPUTS,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Perform the setup for Crow IP Module  Switch devices."""
-    devices = []
-    configured_outputs = discovery_info["outputs"]        
-    _LOGGER.debug(str(configured_outputs))
 
-
-    if configured_outputs != None:
-        for output_num in configured_outputs:
-            device_config_data = OUTPUT_SCHEMA(configured_outputs[output_num])
-            device = CrowIPModuleOutput(
-                hass,
-                output_num,
-                device_config_data[CONF_OUTPUTNAME],
-                hass.data[DATA_CRW].output_state[output_num],
-                hass.data[DATA_CRW],
-            )
-            devices.append(device)
-
-    for relay_num in range(1, 3):
-        device = CrowIPModuleRelay(
-            hass,
-            relay_num,
-            hass.data[DATA_CRW],
-        )
-        devices.append(device)
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the Crow IP Module switches."""
+    controller = hass.data[DOMAIN][entry.entry_id]
+    options = entry.options
     
-    async_add_entities(devices)
+    entities = []
 
-class CrowIPModuleOutput(CrowIPModuleDevice, SwitchEntity):
-    """Representation of an Crow IP Module Output Switch."""
+    # Configured Outputs
+    configured_outputs = options.get(CONF_OUTPUTS, {})
+    
+    # Fallback für Outputs 3 & 4 falls nicht im ConfigFlow (da Nutzer danach fragte)
+    if not configured_outputs:
+         configured_outputs = {
+             "3": {"name": "Modem"},
+             "4": {"name": "Gatewayrouter"}
+         }
 
-    def __init__(self, hass, output_number, output_name, info, controller):
-        """Initialize the switch"""
+    for output_num_str, output_data in configured_outputs.items():
+        output_num = int(output_num_str)
+        name = output_data.get("name", f"Output {output_num}")
+        entities.append(CrowOutput(controller, output_num, name))
+
+    # Relays
+    for relay_num in range(1, 3):
+        entities.append(CrowRelay(controller, relay_num))
+
+    async_add_entities(entities)
+
+
+class CrowBaseSwitch(SwitchEntity):
+    """Base class for Crow Switches."""
+    _attr_has_entity_name = True  # NEW for HA 2025
+
+    def __init__(self, controller):
+        self._controller = controller
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, "crow_alarm_panel")},
+            name="Crow Alarm System",
+            manufacturer="Crow/AAP",
+            model="IP Module",
+            configuration_url=f"http://{self._controller.ip}",
+        )
+
+
+class CrowOutput(CrowBaseSwitch):
+    """Representation of a Crow IP Module Output."""
+
+    def __init__(self, controller, output_number, output_name) -> None:
+        super().__init__(controller)
         self._output_number = output_number
-        _LOGGER.debug("Setting up output switch for system...")
-        super().__init__("Crow " + output_name, info, controller)
-        self._name = "Crow Output " + output_name
-        self._state = STATE_OFF
+        self._attr_name = output_name  # Just "Modem", not "Crow Modem"
+        self._attr_unique_id = f"crow_output_{output_number}"
+        self._is_on = False
+        
+        if self._output_number in self._controller.output_state:
+             self._is_on = self._controller.output_state[self._output_number]["status"]["open"]
 
-    async def async_added_to_hass(self):
-        """Register callbacks."""
-        async_dispatcher_connect(self.hass, SIGNAL_OUTPUT_UPDATE, self._update_callback)
-
-    @property
-    def name(self):
-        """Return name of the output."""
-        return self._name
+    async def async_added_to_hass(self) -> None:
+        self.async_on_remove(
+            async_dispatcher_connect(self.hass, SIGNAL_OUTPUT_UPDATE, self._update_callback)
+        )
 
     @property
-    def is_on(self):
-        """Return true if device is on."""
-        self._state = self._info["status"]["open"]
-        _LOGGER.debug("Is_on="+str(self._state))
-        return self._state
+    def is_on(self) -> bool:
+        return self._is_on
 
-    async def async_turn_on(self, **kwargs):
-        """Turn on the output."""
-        self.hass.data[DATA_CRW].command_output(str(self._output_number))
-        self._state = STATE_ON
-        self.async_schedule_update_ha_state()
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        self._controller.command_output(str(self._output_number))
+        self._is_on = True
+        self.async_write_ha_state()
 
-    async def async_turn_off(self, **kwargs):
-        """Turn off the output."""
-        self.hass.data[DATA_CRW].command_output(str(self._output_number))
-        self._state = STATE_OFF
-        self.async_schedule_update_ha_state()
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        self._controller.command_output(str(self._output_number))
+        self._is_on = False
+        self.async_write_ha_state()
 
     @callback
-    def _update_callback(self, output):
-        """Update the output state in HA, if needed."""
-        if output is None or int(output) == int(self._output_number):
-            self.async_schedule_update_ha_state()
-            _LOGGER.debug("Update poutput"+str(output))
+    def _update_callback(self, output) -> None:
+        if output is None or int(output) == self._output_number:
+            new_state = self._controller.output_state[self._output_number]["status"]["open"]
+            if self._is_on != new_state:
+                self._is_on = new_state
+                self.async_write_ha_state()
 
-class CrowIPModuleRelay(CrowIPModuleDevice, SwitchEntity):
-    def __init__(self, hass, relay_number,controller):
-        """Initialize the switch"""
+
+class CrowRelay(CrowBaseSwitch):
+    """Representation of a Crow IP Module Relay."""
+
+    def __init__(self, controller, relay_number) -> None:
+        super().__init__(controller)
         self._relay_number = relay_number
-        _LOGGER.debug("Setting up relay switch for system...")
-        self._name = "Crow Relay " + str(relay_number)
-        self._state = STATE_OFF
+        self._attr_name = f"Relay {relay_number}"
+        self._attr_unique_id = f"crow_relay_{relay_number}"
+        self._attr_icon = "mdi:electric-switch"
 
     @property
-    def name(self):
-        """Return name of the output."""
-        return self._name
+    def is_on(self) -> bool:
+        return False
 
-    @property
-    def is_on(self):
-        """Return true if device is on."""
-        self._state = False
-        _LOGGER.debug("Is_on="+str(self._state))
-        return self._state
-
-    async def async_turn_on(self, **kwargs):
-        """Trigger relay_1_on."""
-        self.hass.data[DATA_CRW].relay_on(self._relay_number)
-        self.hass.components.logbook.log_entry(
-                'crowipmodule',' Relay ' + str(self._relay_number) +
-                ' is pressed', 'crowipmodule', self.entity_id)
-        self._state = False
-        self.async_schedule_update_ha_state()
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        self._controller.relay_on(self._relay_number)
+        
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        pass
