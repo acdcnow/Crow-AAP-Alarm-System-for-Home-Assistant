28e5639f9b9cc836a2c730a6a18888757a92e6fb
acdcnow <33558339+acdcnow@users.noreply.github.com>
2026-09-14 10:42:42 +0200

    Refactor Crow IP Module sensor for connection updates

--- custom_components/crowipmodule/sensor.py
+++ custom_components/crowipmodule/sensor.py
@@ -5,15 +5,16 @@ from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from homeassistant.helpers.entity import DeviceInfo, EntityCategory
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.const import CONF_HOST
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.const import CONF_HOST, EntityCategory
 
 from .const import (
     DOMAIN,
     SIGNAL_SYSTEM_UPDATE,
     SIGNAL_AREA_UPDATE,
-    CONF_FW_VERSION, CONF_FW_DATE, 
+    SIGNAL_CONNECTION_UPDATE,
+    CONF_FW_VERSION, CONF_FW_DATE,
     DEFAULT_FW_VERSION, DEFAULT_FW_DATE,
     CONF_AREAS, DEFAULT_NUM_AREAS, CONF_NUM_AREAS
 )
@@ -23,7 +24,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+    async_add_entities: AddConfigEntryEntitiesCallback,
 ) -> None:
     """Set up the Crow IP Module sensor."""
     controller = hass.data[DOMAIN][entry.entry_id]
@@ -53,11 +54,19 @@ async def async_setup_entry(
 class CrowBaseTextSensor(SensorEntity):
     _attr_has_entity_name = True
     _attr_should_poll = False
-    
+
     def __init__(self, controller, host, fw_version, fw_date):
         self._controller = controller
         self._host = host
         self._fw_string = f"{fw_version} ({fw_date})"
+
+    @property
+    def available(self) -> bool:
+        return self._controller.is_connected
+
+    @callback
+    def _connection_callback(self, _connected) -> None:
+        self.async_write_ha_state()
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -84,6 +93,9 @@ class CrowSystemSensor(CrowBaseTextSensor):
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(
             async_dispatcher_connect(self.hass, SIGNAL_SYSTEM_UPDATE, self._update_callback)
+        )
+        self.async_on_remove(
+            async_dispatcher_connect(self.hass, SIGNAL_CONNECTION_UPDATE, self._connection_callback)
         )
 
     @property
@@ -119,6 +131,9 @@ class CrowAlarmZoneSensor(CrowBaseTextSensor):
     async def async_added_to_hass(self) -> None:
         self.async_on_remove(
             async_dispatcher_connect(self.hass, SIGNAL_AREA_UPDATE, self._update_callback)
+        )
+        self.async_on_remove(
+            async_dispatcher_connect(self.hass, SIGNAL_CONNECTION_UPDATE, self._connection_callback)
         )
 
     @property
